e7a014dae8126abd88fe4be5bc6388186b2afc10
timeless15 <hsq5218@163.com>
2018-04-06 23:39:38 +0800

    task2-5 finished

--- 2015_spring/task0002/js/task0002_5.js
+++ 2015_spring/task0002/js/task0002_5.js
@@ -1,13 +1,5 @@
-//initial set top value
-var con1 = $(".con")[0];
-var div1s = con1.getElementsByTagName("div");
-var con2 = $(".con")[1];
-var div2s = con2.getElementsByTagName("div");
-for(var i=0,len = div1s.length;i<len;i++){
-    div1s[i].style.top= 60*i + "px";
-    div2s[i].style.top= 60*i + "px";
-}
 //user-select跨浏览器
+//使用mouse事件时，要将draggable设为false默认值，否则会由问题
 function eventDelegate(element, tag, eventName, listener) {
     element.addEventListener(eventName,function(e){
         var event = e || window.event;
@@ -17,25 +9,33 @@ function eventDelegate(element, tag, eventName, listener) {
         }
     })
 }
+
+var conLeft = $(".con")[0];
+var conRight = $(".con")[1];
 /*
 //drop事件应用于被放置的对象
-$(".con")[0].addEventListener("dropenter",dropenter);
-$(".con")[1].addEventListener("dropenter",dropenter);
+conLeft.addEventListener("dropenter",dropenter);
+conRight.addEventListener("dropenter",dropenter);
 //默认元素不能被拖拽进入其他元素
-$(".con")[0].addEventListener("dragover",allowDrop);
-$(".con")[1].addEventListener("dragover",allowDrop);
-$(".con")[0].addEventListener("dragleave",allowDrop);
-$(".con")[1].addEventListener("dragleave",allowDrop);
-$(".con")[0].addEventListener("drop",drop);
-$(".con")[1].addEventListener("drop",drop);
+conLeft.addEventListener("dragover",allowDrop);
+conRight.addEventListener("dragover",allowDrop);
+conLeft.addEventListener("dragleave",allowDrop);
+conRight.addEventListener("dragleave",allowDrop);
+conLeft.addEventListener("drop",drop);
+conRight.addEventListener("drop",drop);
 
 //drag事件应用于被拖拽的对象
-eventDelegate($(".con")[0],"div","dragstart",dragstart);
-eventDelegate($(".con")[1],"div","dragstart",dragstart);
-eventDelegate($(".con")[0],"div","drag",drag);
-eventDelegate($(".con")[1],"div","drag",drag);
-eventDelegate($(".con")[0],"div","dragend",dragend);
-eventDelegate($(".con")[1],"div","dragend",dragend);*/
+eventDelegate(conLeft,"div","dragstart",dragstart);
+eventDelegate(conRight,"div","dragstart",dragstart);
+eventDelegate(conLeft,"div","drag",drag);
+eventDelegate(conRight,"div","drag",drag);
+eventDelegate(conLeft,"div","dragend",dragend);
+eventDelegate(conRight,"div","dragend",dragend);
+//将div的属性值设为drabble
+for(var i=0,len=conLeft.children.length;i<len;i++){
+    conLeft.children[i].setAttribute("draggable",true);
+    conRight.children[i].setAttribute("draggable",true);
+}*/
 
 function dragstart(ev) {
     //dragstart设置的style是拖拽起来的对象
@@ -50,6 +50,8 @@ function dragend(ev){
     //deagend时 drag和dragstart设置的style都会被应用
     ev.target.style.opacity = "1";
     ev.target.style.backgroundColor = "red";
+    initPosition(conLeft);
+    initPosition(conRight);
 }
 function dropenter(ev){
     console.log(ev.target);
@@ -76,17 +78,87 @@ function drop(ev) {
 
 //事件顺序： mousedown 原来容器内的元素remove，同时把元素放到再上一层的容器中，跟随鼠标移动。
 //mouseup： 把元素放append到另一个容器
+
+//initial set top value
+initPosition(conLeft);
+initPosition(conRight);
+eventDelegate(conLeft,"div","mousedown",mouseDown);
+eventDelegate(conRight,"div","mousedown",mouseDown);
+
+function initPosition(block){
+    var childs = block.children;
+    for(var i = 0,len=childs.length;i<len;i++){
+        childs[i].style.top = 60*i + 2 + "px";
+        childs[i].style.left = "";
+    }
+}
+function outOfScreen(e){
+    var maxW = window.innerWidth;
+    var maxH = window.innerHeight;
+    return e.clientX<=0 || e.clientX>=maxW || e.clientY<=0 || e.clientY>=maxH
+}
+function judgeIn(x,y,block){
+    var x0 = getPosition(block).x;
+    var y0 = getPosition(block).y;
+    var x1 = x0 + block.offsetWidth;
+    var y1 = y0 + block.offsetHeight;
+    return x > x0 && x < x1 && y > y0 && y < y1;
+}
 function mouseDown(ev){
     var target = ev.target;
     var parent = target.parentNode;
+    /*鼠标位置
+    var mouseX = ev.clientX;
+    var mouseY = ev.clientY;
+    //div位置
+    var divLeft = target.offsetLeft;
+    var divTop = target.offsetTop;*/
+    var deltaY = target.offsetTop - ev.clientY;
+    var deltaX = target.offsetLeft - ev.clientX;
+
     target.style.opacity = "0.5";
-    parent.removeChild(target);
-    parent.parentNode.appendChild(target);
-    //console.log(target,parent);
+    
+    var firstMove = true;
 
-}
-function mouseMove(ev){
+    document.onmousemove = function(e){
+        if(firstMove){
+            parent.removeChild(target);
+            parent.parentNode.appendChild(target);//target在main下
+        }
+        firstMove = false;
+        var currentX = e.clientX;
+        var currentY = e.clientY;
 
+        if(outOfScreen(e)){
+            target.parentNode.removeChild(target);
+            parent.appendChild(target);
+            initPosition(parent);
+            document.onmousemove = null;
+        }else{
+           target.style.top = currentY + deltaY + "px";
+           target.style.left = currentX + deltaX + "px";
+           initPosition(parent); 
+        }
+    }
+    document.onmouseup = function(e){
+        document.onmousemove = null;
+        document.onmouseup = null;
+
+        target.style.opacity = "1";
+        target.parentNode.removeChild(target);
+
+        if(judgeIn(e.clientX,e.clientY,conLeft)){
+            conLeft.appendChild(target);
+            initPosition(conLeft);
+        }
+        else if(judgeIn(e.clientX,e.clientY,conRight)){
+            conRight.appendChild(target);
+            initPosition(conRight);
+        }
+        else{
+            parent.appendChild(target);
+            initPosition(parent);
+        }
+    }
+    return false;
 }
-eventDelegate($(".con")[0],"div","mousedown",mouseDown);
-$(".main")[0].addEventListener("mousemove",mouseMove);
