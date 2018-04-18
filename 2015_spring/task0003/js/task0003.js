bc47efd4bd3d1bdb04cc87d78ad79b79bc9ba9a4
Han,Shiqi <hsq5218@163.com>
2018-04-18 14:36:57 +0800

    task3 finish

--- 2015_spring/task0003/js/task0003.js
+++ 2015_spring/task0003/js/task0003.js
@@ -1,87 +1,88 @@
 
 /*database*/
 function initDataBase(){
-	if(!localStorage.item || !localStorage.subitem || !localStorage.task){
-		var itemJson = [{
-		    "id":0,
-		    "name":"默认分类",
-		    "child":[0]
-		},{
-		    "id": 1,
-		    "name": "工作",
-		    "child": [1, 2]
-		},{
-		    "id": 2,
-		    "name": "生活",
-		    "child": [3]
-		}];
-		var subItemJson = [{
-		    "id":0,
-		    "pid":0,
-		    "name":"默认子分类",
-		    "child":[0]
-		},{
-		    "id": 1,
-		    "pid": 1,
-		    "name": "前端",
-		    "child": [1,2,3]
-		},{
-		    "id": 2,
-		    "pid": 1,
-		    "name": "服务端",
-		    "child": [4]
-		},{
-		    "id": 3,
-		    "pid": 2,
-		    "name": "吃饭",
-		    "child": [],
-		}];
-		var taskJson = [{
-		    "id":0,
-		    "pid":0,
-		    "finish":false,
-		    "name": "Task",
-		    "date":"2015-06-05",
-		    "text":"This is a task"
-		},{
-		    "id":1,
-		    "pid":1,
-		    "finish":true,
-		    "name": "IFE",
-		    "date": "2015-05-10",
-		    "text": "百度ife任务1",
-		},{
-		    "id": 2,
-		    "pid": 1,
-		    "finish": false,
-		    "name": "drools",
-		    "date": "2015-05-31",
-		    "text": "研究drools推理引擎",
-		},{
-		    "id": 3,
-		    "pid": 1,
-		    "finish": true,
-		    "name": "Sass",
-		    "date": "2015-06-31",
-		    "text": "学习慕课网的视频Sass", 
-		},{
-		    "id": 4,
-		    "pid": 2,
-		    "finish": false,
-		    "name":  "AMD",
-		    "date": "2015-07-31",
-		    "text": "学习AMD",        
-		}];
-	localStorage.item = JSON.stringify(itemJson);
-	localStorage.subitem = JSON.stringify(subItemJson);
-	localStorage.task = JSON.stringify(taskJson);
-	}
+    if(!localStorage.item || !localStorage.subitem || !localStorage.task){
+        var itemJson = [{
+            "id":0,
+            "name":"默认分类",
+            "child":[0]
+        },{
+            "id": 1,
+            "name": "工作",
+            "child": [1, 2]
+        },{
+            "id": 2,
+            "name": "生活",
+            "child": [3]
+        }];
+        var subItemJson = [{
+            "id":0,
+            "pid":0,
+            "name":"默认子分类",
+            "child":[0]
+        },{
+            "id": 1,
+            "pid": 1,
+            "name": "前端",
+            "child": [1,2,3]
+        },{
+            "id": 2,
+            "pid": 1,
+            "name": "服务端",
+            "child": [4]
+        },{
+            "id": 3,
+            "pid": 2,
+            "name": "吃饭",
+            "child": [],
+        }];
+        var taskJson = [{
+            "id":0,
+            "pid":0,
+            "finish":false,
+            "name": "Task",
+            "date":"2015-06-05",
+            "text":"This is a task"
+        },{
+            "id":1,
+            "pid":1,
+            "finish":true,
+            "name": "IFE",
+            "date": "2015-05-10",
+            "text": "百度ife任务1",
+        },{
+            "id": 2,
+            "pid": 1,
+            "finish": false,
+            "name": "drools",
+            "date": "2015-05-31",
+            "text": "研究drools推理引擎",
+        },{
+            "id": 3,
+            "pid": 1,
+            "finish": true,
+            "name": "Sass",
+            "date": "2015-06-31",
+            "text": "学习慕课网的视频Sass", 
+        },{
+            "id": 4,
+            "pid": 2,
+            "finish": false,
+            "name":  "AMD",
+            "date": "2015-07-31",
+            "text": "学习AMD",        
+        }];
+    localStorage.item = JSON.stringify(itemJson);
+    localStorage.subitem = JSON.stringify(subItemJson);
+    localStorage.task = JSON.stringify(taskJson);
+    }
 }
 
 
 //新增item/subitem->后台数据库更新->
 //新增task->后台数据库更新->
 //->渲染整个页面item/subite/task/detail
+
 var allTask = $(".allTask");
 var itemUl = $(".item-ul");
 var taskList = $(".task-list");
@@ -90,15 +91,18 @@ var currentItemID = 0;
 var currentSubitemID = 0;
 var currentTaskID = 0;
 var showTaskId = [];
-var itemJson = JSON.parse(localStorage.item);
-var subItemJson = JSON.parse(localStorage.subitem);
-var taskJson = JSON.parse(localStorage.task);
+var itemJson,subItemJson,taskJson;
+initial();
 function initial(){
-	initDataBase();
+    initDataBase();
+    itemJson = JSON.parse(localStorage.item);
+    subItemJson = JSON.parse(localStorage.subitem);
+    taskJson = JSON.parse(localStorage.task);
     allTask.innerHTML = taskJson.length;
     for(var i=0,len=itemJson.length;i<len;i++){
         renderItem(itemJson[i].id);
     }
+    addClass($(".item-head"),"active")
     renderTaskAll();
     renderContent(taskJson[0].id);
     //默认分类不能删除
@@ -106,7 +110,7 @@ function initial(){
     btns[0].parentNode.removeChild(btns[0]);
     btns[0].parentNode.removeChild(btns[0]);//删掉后btns更新
 }
-initial();
+
 /*render function*/
 //渲染主分类
 function renderItem(itemID){
@@ -156,7 +160,9 @@ function renderTaskAll(){
     for(var i=0,len=taskJson.length;i<len;i++){
         showTaskId.push(taskJson[i].id);
     }
+    sortDate();
     renderTask();
+    groupDate();
     var task0Span = queryTaskDOMById(showTaskId[0]);
     addClass(task0Span,"select");
     renderContent(taskJson[0].id);
@@ -263,13 +269,17 @@ function itemClick(e){
             showTaskId.push(subitem.child[j])
         }
     }
+    sortDate();
     renderTask();
+    groupDate();
     //显示第一个子分类第一个task的内容
     var task0ID = querySubitemById(childArr[0]).child[0];
     var task0Span = queryTaskDOMById(task0ID);
     addClass(task0Span,"select");
     renderContent(task0ID);
-
+    addClass($(".task-filter .all"),"select");
+    removeClass($(".task-filter .finish"),"select");
+    removeClass($(".task-filter .unfinish"),"select");
 }
 //subitem点击事件
 function subitemClick(e){
@@ -285,12 +295,19 @@ function subitemClick(e){
     for (var i=0,len=subitem.child.length;i<len;i++){
         showTaskId.push(subitem.child[i]);
     }
+    sortDate();
     renderTask();
-    //显示第一个task的内容
-    var task0ID = subitem.child[0];
-    var task0Span = queryTaskDOMById(task0ID);
-    addClass(task0Span,"select");
-    renderContent(task0ID);
+    groupDate();
+    //显示第一个task的内容,没有则显示空
+    if(subitem.child.length>0){
+        var task0ID = subitem.child[0];
+        var task0Span = queryTaskDOMById(task0ID);
+        addClass(task0Span,"select");
+        renderContent(task0ID);
+    }
+    addClass($(".task-filter .all"),"select");
+    removeClass($(".task-filter .finish"),"select");
+    removeClass($(".task-filter .unfinish"),"select");
 }
 
 function taskClick(e){
@@ -338,7 +355,7 @@ $(".confirm-item").addEventListener("click",function(e){
         renderSubitem(subitemAdd.id);
     }
     localStorage.item = JSON.stringify(itemJson);
-	localStorage.subitem = JSON.stringify(subItemJson);
+    localStorage.subitem = JSON.stringify(subItemJson);
     modal.style.display = "none";
 });
 
@@ -364,6 +381,10 @@ $(".save-task").addEventListener("click",function(e){
         task.name = $("input.in-taskname").value;
         task.date = $("input.in-taskdate").value;
         task.text = $("textarea.in-tasktext").value;
+        taskList.innerHTML = "";
+        sortDate();
+        renderTask();
+        groupDate();
     }else{
         //先判断加入到哪个子分类
         subitemID = getSelectSubitem();
@@ -387,7 +408,9 @@ $(".save-task").addEventListener("click",function(e){
         //中间栏task列表更新
         taskList.innerHTML = "";
         showTaskId.push(taskID);
+        sortDate();
         renderTask();
+        groupDate();
         removeClass(getSelectTask(),"select");
         addClass(queryTaskDOMById(taskID),"select");
     }   
@@ -400,8 +423,8 @@ $(".save-task").addEventListener("click",function(e){
     $("input.in-taskdate").value="";
     $("textarea.in-tasktext").value="";
     localStorage.item = JSON.stringify(itemJson);
-	localStorage.subitem = JSON.stringify(subItemJson);
-	localStorage.task = JSON.stringify(taskJson);
+    localStorage.subitem = JSON.stringify(subItemJson);
+    localStorage.task = JSON.stringify(taskJson);
 });
 
 $(".cancel-task").addEventListener("click",function(e){
@@ -452,8 +475,8 @@ eventDelegate(itemUl,"button","click",function(e){
         allTask.innerHTML = taskJson.length;
         renderTaskAll();
         localStorage.item = JSON.stringify(itemJson);
-		localStorage.subitem = JSON.stringify(subItemJson);
-		localStorage.task = JSON.stringify(taskJson);
+        localStorage.subitem = JSON.stringify(subItemJson);
+        localStorage.task = JSON.stringify(taskJson);
     }
 })
 
@@ -480,7 +503,7 @@ eventDelegate($(".show-content"),"button","click",function(e){
             renderContent(taskid);
         } 
     };
-	localStorage.task = JSON.stringify(taskJson);
+    localStorage.task = JSON.stringify(taskJson);
 })
 function getSelectFilter(){
     var filterLi = $(".task-filter").getElementsByTagName("li");
@@ -503,13 +526,25 @@ eventDelegate($(".task-filter"),"li","click",function(){
             currentTaskListAll.push(taskJson[i].id);
         }
     }else{
-        var subitemID = getSelectSubitem();
-        var subitem = querySubitemById(subitemID);
-        currentTaskListAll = subitem.child;
+        var current = getSelectItem();
+        if(hasClass(current,"item-li")){
+            var item = queryItemById(parseInt(current.getAttribute("itemid")));
+            for(var i=0,len=item.child.length;i<len;i++){
+                var subitem = querySubitemById(item.child[i]);
+                for(var j=0,sublen=subitem.child.length;j<sublen;j++){
+                    currentTaskListAll.push(subitem.child[j]);
+                }
+            }
+        }else if(hasClass(current,"subitem-li")){
+            var subitem = querySubitemById(parseInt(current.getAttribute("subitemid")));
+            currentTaskListAll = subitem.child;
+        }
     }
     showTaskId = [];
     showTaskId = taskFilter(currentTaskListAll,tag);
+    sortDate();
     renderTask();
+    groupDate();
 })
 
 function taskFilter(currentTaskList,tag){
@@ -525,7 +560,42 @@ function taskFilter(currentTaskList,tag){
     if(tag=="finish") return finishArr;
     if(tag=="unfinish") return unfinishArr;
 }
-
+/*sort by date*/
+function sortDate(){
+    var temptask = [];
+    for(var i=0,len=showTaskId.length;i<len;i++){
+        temptask.push(queryTaskById(showTaskId[i]));
+    }
+    temptask.sort(function(a,b){
+        var c = a.date.split("-");
+        var d = b.date.split("-");
+        if(parseInt(c[0])>parseInt(d[0])) return -1;
+        else if(parseInt(c[0])<parseInt(d[0])) return 1;
+        else if(parseInt(c[1])>parseInt(d[1])) return -1;
+        else if(parseInt(c[1])<parseInt(d[1])) return 1;
+        else if(parseInt(c[2])>parseInt(d[2])) return -1;
+        else if(parseInt(c[2])<parseInt(d[2])) return 1;
+        else return 0;
+    });
+    showTaskId = [];
+    for(var i=0,len=temptask.length;i<len;i++){
+        showTaskId.push(temptask[i].id);
+    }
+}
+function groupDate(){
+    var temptask = [];
+    for(var i=0,len=showTaskId.length;i<len;i++){
+        temptask.push(queryTaskById(showTaskId[i]));
+    }
+    for(var i=0,len=temptask.length-1;i<len;i++){
+        var now = temptask[i].date;
+        var next= temptask[i+1].date;
+        if(now==next) {
+            var taskTimeSpan = queryTaskDOMById(temptask[i+1].id).previousSibling;
+            taskTimeSpan.parentNode.removeChild(taskTimeSpan);
+        }
+    }
+}
 //Common Function
 function queryItemById(itemID){
     for(var i=0,len = itemJson.length;i<len;i++){
@@ -583,4 +653,3 @@ function arrayDelete(id,array){
     }
     return array.splice(index,1);
 }
-//TODO:delete*,check for edit or finish*,finish-filter*,localStorage*,sortByDate,
