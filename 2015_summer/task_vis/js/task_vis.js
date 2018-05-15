bad35a92a2442824666e4b1b98035071d2ccb305
Han,Shiqi <hsq5218@163.com>
2018-05-15 17:21:12 +0800

    Update task_vis.js

--- 2015_summer/task_vis/js/task_vis.js
+++ 2015_summer/task_vis/js/task_vis.js
@@ -1,4 +1,4 @@
-var data = [{
+var testdata = [{
 	"month":"Jan",
 	"AQI":[9,8,8,7]
 },{
@@ -8,12 +8,6 @@ var data = [{
 	"month":"Jan",
 	"AQI":[8,7,9,7]
 }];
-var vinyls = {
-	"classic":10,
-	"rock":14,
-	"pop":2,
-	"Jazz":12
-};
 //var canvas = document.getElementById("AQICanvas");
 function drawLine(ctx,startX,startY,endX,endY,color){
 	ctx.save();
@@ -31,41 +25,56 @@ function drawBar(ctx,x,y,width,height,color){
 	ctx.fillRect(x,y,width,height);
 	ctx.restore();
 }
+function addText(ctx,color,x,y,text){
+	ctx.save();
+	ctx.fillStyle = color;
+	ctx.font = "16px Arial"
+	ctx.fillText(text,x,y);
+	ctx.restore(); 
+}
 var Barchart = function(options){
-	this.options = options;
 	this.canvas = options.canvas;
 	this.ctx = this.canvas.getContext("2d");
 	this.colors = options.colors;
+	this.data = options.data;
 	this.draw = function(){
-		var maxValue = 0;
-		var data = this.options.data;
-		for(var categ in data){
-			maxValue = Math.max(maxValue,data[categ]);
-		}
-		var canvasActualHeight = this.canvas.height-this.options.padding*2;
-		var canvasActualWidth = this.canvas.width-this.options.padding*2;
-		var barIndex = 0;
-		var numberOfBars = Object.keys(data).length;
-		var barSize = canvasActualWidth/numberOfBars;
-		for(categ in data){
-			var val = data[categ];
-			var barHeight = Math.round(canvasActualHeight*val/maxValue);
-			drawBar(
-				this.ctx,
-				this.options.padding+barIndex*barSize,
-				this.canvas.height - barHeight - this.options.padding,
-				barSize,
-				barHeight,
-				this.colors[barIndex%this.colors.length]
-			);
-			barIndex++;
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);//clear for redraw
+		var padding = options.padding,
+			canvasWidth = this.canvas.width - padding * 2,
+			canvasHeight = this.canvas.height - padding * 2,
+			barIndex = this.data.length,
+			barHeight = Math.round(canvasHeight / barIndex),
+			initialY = padding;
+		for (var i=0;i<barIndex;i++) {
+			var AQIdata = this.data[i].AQI,
+				month = this.data[i].month,
+				initialX = padding + 30;
+			addText(this.ctx, "#000", padding , initialY + 20, month);
+			for(var j=0;j<AQIdata.length;j++){
+				drawBar(this.ctx, initialX, initialY +5 , 10 * AQIdata[j], barHeight-10, this.colors[j]);
+				addText(this.ctx, "#fff", initialX + 10, initialY + 22, AQIdata[j]);
+				initialX += 10 * AQIdata[j];
+			}
+			initialY += barHeight;
 		}
 	}
 };
 var myBarchart = new Barchart({
 	canvas:document.getElementById("AQICanvas"),
 	padding:10,
-	data:vinyls,
+	data: testdata,
 	colors:["#a55ca5","#67b6c7","#bccd7a","#eb9743"]
 });
 myBarchart.draw();
+
+var checkboxs = document.querySelectorAll("input[name=type]");
+var checkArr = [1,1,1,1];
+for(var i=0,len=checkboxs.length;i<len;i++){
+	checkboxs[i].addEventListener("change",function(){
+		if(this.checked){
+			checkArr[]=1;
+		}else{
+			checkArr[]=0;
+		}console.log(checkArr)
+	})
+}
